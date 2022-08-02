e60a2d4c952a512201fcf38a563b588684260799
David Schneeberger <david.schneeberger@adnovum.ch>
2022-08-02 17:32:07 +0200

    Add windows batch file

---
+++ Dependencies/IPlug/download-iplug-sdks.bat
@@ -0,0 +1,18 @@
+
+RD /S /Q "VST3_SDK"
+RD /S /Q "WAM_AWP"
+RD /S /Q "WAM_SDK"
+
+git clone https://github.com/iplug2/audioworklet-polyfill WAM_AWP
+git clone https://github.com/iplug2/api.git WAM_SDK
+
+git clone https://github.com/steinbergmedia/vst3sdk.git VST3_SDK
+cd VST3_SDK
+git submodule update --init pluginterfaces
+git submodule update --init base
+git submodule update --init public.sdk
+git submodule update --init doc
+cd ..
+git checkout ./VST3_SDK/README.md
+git checkout ./WAM_SDK/readme.txt
+git checkout ./WAM_AWP/readme.txt
